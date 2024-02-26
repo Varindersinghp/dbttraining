fe2742272c335a63a20f35b7c4cc05f817c84aee
Varinder Singh <varinderpatwal2@gmail.com>
2024-02-26 11:20:04 +0000

    Commit

--- models/salesmart/dim_date.sql
+++ models/salesmart/dim_date.sql
@@ -1,3 +1,3 @@
-{{config(materialized='view' , schema='salesmart' )}}
+{{config(materialized='view' , schema=env_var('DBT_SALESMARTDB','SALESMART') )}}
 
 {{dbt_date.get_date_dimension(get_order_mindate(),get_order_maxdate())  }}
